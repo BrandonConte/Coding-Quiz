4f0e60945bb00206c89e6b51c29a761f427135a1
BrandonConte <bconte950@gmail.com>
2021-10-14 19:21:32 -0400

    added in variables that are shown in the index.
    Also set score and questionInfo var to = 0

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Declaring variables
+var score = 0;
+var questionInfo = 0;
+
+// More declared variables
+var timeLeft = document.querySelector("#timeLeft");
+var startQuiz = document.querySelector("#startQuiz");
+var wrapper = document.querySelector("#wrapper");
+var questions = document.querySelector("#questions");
+

085b98f8b9fae94cb2e51ba77cdb382ce28baa16
BrandonConte <bconte950@gmail.com>
2021-10-14 19:30:51 -0400

    added in variables for quiz timers/penalty/interval/button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@ var startQuiz = document.querySelector("#startQuiz");
 var wrapper = document.querySelector("#wrapper");
 var questions = document.querySelector("#questions");
 
+// Total time of the quiz
+var secondsLeft = 81;
+
+// Penalty timer - Ticks off this amount of time in seconds if question is answered incorrectly. ( in this case 5 seconds)
+var penalty = 5;
+
+// Interval timer - Will be added below with if statements
+var timeInterval = 0;
+
+
+
+// Starts timer with button click -- 
+startQuiz.addEventListener("click", function() {
+    
+})

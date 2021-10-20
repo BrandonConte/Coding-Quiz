8080b056cbc5415d74e2419e5b4e84d76b1568a4
BrandonConte <bconte950@gmail.com>
2021-10-20 19:07:03 -0400

    fixed timer to function properly and styled it again

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var score = 0;
 var questionInfo = 0;
 
 // More declared variables
-var timeLeft = document.querySelector("#timeLeft");
+var timerLeft = document.querySelector("#timerLeft");
 var startQuiz = document.querySelector("#startQuiz");
 var wrapper = document.querySelector("#wrapper");
 var questions = document.querySelector("#questions");
@@ -37,33 +37,37 @@ var multiChoice = [
     },
 ]
 
-// Total time of the quiz
-var secondsLeft = 81;
-
 // Penalty timer - Ticks off this amount of time in seconds if question is answered incorrectly. ( in this case 5 seconds)
-var penalty = 5;
-
-// Interval timer - Will be added below with if statements
-var timeInterval = 0;
+var penalty = 10;
 
 // Creates new element
 var ulNew = document.createElement("ul");
 
+function countdown () {
+
+    var timeLeft = 80;
+
+    var timeInterval = setInterval(function() {
+        
+        if (timeLeft > 1) {
+            timerLeft.textContent = timeLeft + " seconds remaining";
+
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerLeft.textContent = timeLeft + "seconds remaining";
+            timeLeft--;
+
+        } else {
+            timerLeft.textContent = "";
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+    }, 1000);
+}
+
 // Starts timer with button click -- 
 startQuiz.addEventListener("click", function() {
-    
-    if (timeInterval === 0) {
-        timeInterval = setInterval(function() {
-            secondsLeft--;
-            timeLeft.textContent = "Time:" + secondsLeft;
-            // When time hits 0 will end quiz and say times up
-            if (secondsLeft = 0) {
-                clearInterval(timeInterval);
-                timeLeft.textContent = "Time's up";
-            }
-        }, 1000);
-    }
-    
+countdown();
 })
 
 function render(questionInfo) {
